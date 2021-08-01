b6a9e62889e5b3d2f0d94dd3cf6aaa6fe5f796cf
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-08-01 15:30:37 +0530

    commit-2
    added cart page

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+apiKey: "AIzaSyCzmVFUmRFfm_5_GMXcBkzKT7sNTW-4sSA",
+authDomain: "clone-db68d.firebaseapp.com",
+projectId: "clone-db68d",
+storageBucket: "clone-db68d.appspot.com",
+messagingSenderId: "691987832693",
+appId: "1:691987832693:web:b1ae24e7ee6d27370ebd3c",
+measurementId: "G-9X9J0XWEZ2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
